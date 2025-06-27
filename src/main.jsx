aaed3b865464deca0416c273c43e2884ef441c4f
Mohamed-Ahmed-Salah <mohameda249@hotmail.com>
2025-06-27 13:16:49 +0300

    fixing assets path and production problems

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="*" element={<Home />} /> 
       </Routes>
     </BrowserRouter> 
   </StrictMode>,
